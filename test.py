248c85130c322d31f2c92130075a7b73b77bb54c
Fede Castelli <feder0210@gmail.com>
2026-07-09 18:26:17 +0200

    constr and sol

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ def solve_IFS(data):
     x = model.addvars(data['I'], data['K'], vtype=GRB.BINARY, name="x")
 
     obj = gb.LinExpr()
+
     for i in data['I']:
         p_to = sum(data['p'][j,i] for j in data['I'])
         p_from = sum(data['p'][i,j] for j in data['I'])
@@ -25,7 +26,44 @@ def solve_IFS(data):
             obj.addTerms(c, x[i,k])
 
     model.setObjective(obj,GRB.MINIMIZE)
-            
+
+    for i in data['I']:
+        model.addConstr(gb.quicksum(x[i,k] for k in data['K_g'][i]) == 1, name = f"constr_3")
+
+    for clique_idx, clique in enumerate(data['T_D']):
+        for k in data['K_fixed']:
+            model.addConstr(gb.quicksum(x[i,k] for i in clique) <= 1, name = f"constr_4")       
+
+    for clique_idx, clique in enumerate(data['T_I']):
+        for k in data['K_fixed']:
+            model.addConstr(gb.quicksum(x[i,k] for i in clique) <= 1, name = f"constr_5") 
+
+    model.addConstr(gb.quicksum(x[i,data['apron']] for i in data['I']) == data['NA'], name = f"constr_6")
+
+    model.optimize()        
+
+    if model.Status == GRB.OPTIMAL:
+        print("IFS' optimum found")
+        sol = {i: k for i in data['I'] for k in data['K'] if x[i,k].X > 0.5}
+
+        real_wd = 0
+        for i in data['I']:
+            for j in data['I']:
+                gate_i = sol[i]
+                gate_j = sol[j]
+                real_wd += data['p'][i,j] * data['d'][gate_i,gate_j]
+
+        for i in data['I']:
+            gate_i = sol[i]
+            real_wd += (data['e'][i] + data['f'][i]) * data['ed'][gate_i]
+
+        return real_wd, sol
+    
+    else:
+        print("No solution found")
+        return None, None
+
+         
 
 
 
