bb1fdf02d944577add1dad802ff931c399e2a613
ElisabettaGrandi <elisabettagrandi03@gmail.com>
2026-07-18 09:57:24 +0200

    fixes

--- data_test.py
+++ data_test.py
@@ -93,6 +93,7 @@ def esegui_test_di_fattibilita():
     seed = 43
     print(f"Generazione istanza iniziale con {num_pazienti} pazienti (Seed: {seed})...")
     istanza_iniziale = get_dataset(group_type="B", num_patients=num_pazienti, seed=seed)
+    #istanza_iniziale = get_dataset_try(group_type="B", num_patients=num_pazienti, seed=seed)
     
     print("\n" + "=" * 70)
     print(" FASE 1: TEST MODELLO ORIGINALE SU DATASET INIZIALE")
